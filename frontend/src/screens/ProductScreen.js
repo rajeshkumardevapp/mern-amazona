21a70351f2a45d57af868c749f3bc18d5e1f0d2d
J Brown <j.b.fitterman@gmail.com>
2022-04-18 02:35:58 -0400

    Video-06-initialize Routes

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+function ProductScreen() {
+  //gives access to params
+  const params = useParams();
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+}
+
+export default ProductScreen;
